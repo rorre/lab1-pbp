636810eb60f51684a5a9e25b011e6c7dfb7eec23
Ren <renrror@gmail.com>
2022-09-15 13:02:10 +0700

    Reformat project with black

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from wishlist.views import show_id_json, show_id_xml, show_wishlist, show_wishlist_json, show_wishlist_xml
 
-app_name = 'wishlist'
+from wishlist.views import (
+    show_id_json,
+    show_id_xml,
+    show_wishlist,
+    show_wishlist_json,
+    show_wishlist_xml,
+)
+
+app_name = "wishlist"
 
 urlpatterns = [
-    path('', show_wishlist, name='show_wishlist'),
-    path('xml/', show_wishlist_xml, name="show_wishlist_xml"),
-    path('json/', show_wishlist_json, name="show_wishlist_json"),
-    path('xml/<int:id>', show_id_xml, name="show_id_xml"),
-    path('json/<int:id>', show_id_json, name="show_id_json"),
+    path("", show_wishlist, name="show_wishlist"),
+    path("xml/", show_wishlist_xml, name="show_wishlist_xml"),
+    path("json/", show_wishlist_json, name="show_wishlist_json"),
+    path("xml/<int:id>", show_id_xml, name="show_id_xml"),
+    path("json/<int:id>", show_id_json, name="show_id_json"),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -10,19 +10,30 @@ def show_wishlist(request: HttpRequest):
     ctx = {"list_barang": items, "nama": "Ren"}
     return render(request, "wishlist.html", ctx)
 
+
 def show_wishlist_xml(request: HttpRequest):
     data = BarangWishlist.objects.all()
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )
+
 
 def show_wishlist_json(request: HttpRequest):
     data = BarangWishlist.objects.all()
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
+
 
 def show_id_json(request: HttpRequest, id: int):
     data = BarangWishlist.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    return HttpResponse(
+        serializers.serialize("json", data), content_type="application/json"
+    )
+
 
 def show_id_xml(request: HttpRequest, id: int):
     data = BarangWishlist.objects.filter(pk=id)
-    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
-    
+    return HttpResponse(
+        serializers.serialize("xml", data), content_type="application/xml"
+    )

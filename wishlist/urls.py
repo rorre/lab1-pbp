19355898646e3135afca35220d1908f984f4ee9f
Ren <renrror@gmail.com>
2022-09-15 12:43:23 +0700

    Add xml endpoint

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist, show_wishlist_xml
 
 app_name = 'wishlist'
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('xml/', show_wishlist_xml, name="show_wishlist_xml"),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.core import serializers
 
 from wishlist.models import BarangWishlist
 
@@ -8,3 +9,9 @@ def show_wishlist(request: HttpRequest):
     items = BarangWishlist.objects.all()
     ctx = {"list_barang": items, "nama": "Ren"}
     return render(request, "wishlist.html", ctx)
+
+def show_wishlist_xml(request: HttpRequest):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+

dfcd16e04f14f40640bb0e887fd37d80f9fd41bf
Ren <renrror@gmail.com>
2022-09-22 09:24:43 +0700

    Implement lab 3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,12 +1,8 @@
 from django.urls import path
 
-from wishlist.views import (
-    show_id_json,
-    show_id_xml,
-    show_wishlist,
-    show_wishlist_json,
-    show_wishlist_xml,
-)
+from wishlist.views import (login_user, logout_user, register, show_id_json,
+                            show_id_xml, show_wishlist, show_wishlist_json,
+                            show_wishlist_xml)
 
 app_name = "wishlist"
 
@@ -16,4 +12,7 @@ urlpatterns = [
     path("json/", show_wishlist_json, name="show_wishlist_json"),
     path("xml/<int:id>", show_id_xml, name="show_id_xml"),
     path("json/<int:id>", show_id_json, name="show_id_json"),
+    path("register/", register, name="register"),
+    path("login/", login_user, name="login"),
+    path("logout/", logout_user, name="logout"),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,13 +1,25 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+import datetime
+
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.core import serializers
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from wishlist.models import BarangWishlist
 
 
+@login_required(login_url="/wishlist/login")
 def show_wishlist(request: HttpRequest):
     items = BarangWishlist.objects.all()
-    ctx = {"list_barang": items, "nama": "Ren"}
+    ctx = {
+        "list_barang": items,
+        "nama": "Ren",
+        "last_login": request.COOKIES["last_login"],
+    }
     return render(request, "wishlist.html", ctx)
 
 
@@ -37,3 +49,40 @@ def show_id_xml(request: HttpRequest, id: int):
     return HttpResponse(
         serializers.serialize("xml", data), content_type="application/xml"
     )
+
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Akun telah berhasil dibuat!")
+            return redirect("wishlist:login")
+
+    context = {"form": form}
+    return render(request, "register.html", context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("wishlist:show_wishlist"))
+            response.set_cookie("last_login", str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, "Username atau Password salah!")
+    context = {}
+    return render(request, "login.html", context)
+
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse("wishlist:login"))
+    response.delete_cookie("last_login")
+    return response
